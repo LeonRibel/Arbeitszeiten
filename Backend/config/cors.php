eff95928c1ba447a9bd663469bd16de8d5d0e38e
Leon <l.ribel@ape-dev.com>
2026-02-06 15:05:36 +0100

    cors

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8003', 'http://localhost:5001', 'http://localhost:8080','http://api.timebase.ape-dev.de'],
+    'allowed_origins' => ['http://127.0.0.1:8003', 'http://localhost:5001', 'http://localhost:8080','http://timebase.ape-dev.de'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Set-Cookie'],
